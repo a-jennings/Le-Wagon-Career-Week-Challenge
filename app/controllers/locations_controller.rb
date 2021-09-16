a8978870e1888b89433083f17600c6329ef27cd2
Drew Jennings <andrew_jennings@ymail.com>
2021-09-16 12:43:26 +0100

    app works but something wrong with api call

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,37 @@
+class LocationsController < ApplicationController
+    require 'json'
+    require 'open-uri'
+    
+    def new
+        @location = Location.new
+    end
+    
+    def create
+        @location = Location.create(location_params)
+        redirect_to location_path(@location.id)
+    end
+    
+    def show
+        @location = Location.find(params[:id])
+        @museums = find_museums(@location)
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:longitude, :latitude)
+    end
+
+    def find_museums(location)
+        longitude = location.longitude
+        latitude = location.latitude
+        url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?proximity=#{longitude},#{latitude}&access_token=pk.eyJ1IjoiYS1qZW5uaW5ncyIsImEiOiJja3RtcXBydDYwMWhlMm9uYm1lemEwNzZ1In0.ACkyNnU4NE8FNrVnDzWpyg"
+        location_serialized = URI.open(url).read
+        museum_locations = JSON.parse(location_serialized)      
+        museum_locations['features'].map do |location|
+            location['place_name']
+        end
+    end
+
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+    def home
+        @location = Location.new
+    end
+end
