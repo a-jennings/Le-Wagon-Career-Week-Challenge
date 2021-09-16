4ccf9a61a890969c57625da9fed6d942c5c49020
Drew Jennings <andrew_jennings@ymail.com>
2021-09-16 11:15:36 +0100

    create Location model

---
+++ db/migrate/20210916101037_create_locations.rb
@@ -0,0 +1,9 @@
+class CreateLocations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :locations do |t|
+      t.float :longitude
+      t.float :latitude
+      t.timestamps
+    end
+  end
+end
